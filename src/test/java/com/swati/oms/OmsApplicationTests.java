e231631ad0eb7c9e0bae0aa4ca61c5beaf17a905
SWATI AHUJA <SWATIAHUJA14@GMAIL.COM>
2022-10-18 23:14:19 +0530

    First Commit

---
+++ src/test/java/com/swati/oms/OmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swati.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
